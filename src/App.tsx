323383984e1e7ebfa578881013e7f93c2d357fe4
TaeWon Park <thisis_tw94@naver.com>
2023-03-22 21:00:45 +0900

    typeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, {keyframes} from "styled-components";
+
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => props.theme.backgroundColor}};
+  
+`
+
+const Title = styled.h1`
+  color : ${props => props.theme.textColor}
+`
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Wrapper>
+      <Title>daram</Title>
+    </Wrapper>
+
   );
 }
-
 export default App;
