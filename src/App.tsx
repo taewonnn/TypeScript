74fad1b5cbac45534deb2e3527012beb52d35a82
TaeWon Park <thisis_tw94@naver.com>
2023-03-28 21:17:29 +0900

    # 4.1 Router v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,10 @@
-// import Circle from "./Circle";
+import Router from "./screens/Router";
 
-import {useState} from "react";
-import styled from "styled-components";
-
-
-
-// TypeScript
-// const plus = (a:number, b:number) => a + b
-
-interface DummyProps {
-  text: string;
-  active?: boolean;
-}
-function Dummy({text, active = false}: DummyProps) {
-  return <h1>{text}</h1>
-}
-
-
-
-const Container = styled.div`
-    background-color: ${props => props.theme.bgColor};
-  `
-
-const H1 = styled.h1`
-  color: ${props => props.theme.textColor}
-`
-
-
-  function App() {
-
-  const [value, setValue] = useState('')
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    console.log(event.currentTarget.value);
-    const {
-      currentTarget : {value},
-    } = event;
-    setValue(event.currentTarget.value);
-  }
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value)
-  }
-
-
-
+function App() {
 
   return (
     <div>
-      <form onSubmit={onSubmit} >
-        <input onChange={onChange} value = {value} type='text' placeholder='username'/>
-        <button>LogIn</button>
-      </form>
-      <Container>
-        <H1>protected</H1>
-      </Container>
-      <Dummy text="hello" active={true} />
+      <Router />
     </div>
 
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {ThemeProvider} from "styled-components";
-import {darkTheme, lightTheme} from "./theme";
+
 
 
 
@@ -11,9 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
