9b97b9676ec0e4aff07f4b239e6e5481c1be9f79
TaeWon Park <thisis_tw94@naver.com>
2023-03-31 20:58:52 +0900

    #4.8 useSearchParams

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,18 @@
 import {users} from "../db";
-import {Link} from "react-router-dom";
+import {Link, useSearchParams} from "react-router-dom";
 
 export default function Home() {
+  const [ readSearchParams, setSearchParams ] = useSearchParams();
+  console.log(readSearchParams.has('geo'))
+
+  // setTimeout(() => {
+  //   setSearchParams({
+  //     day: 'today',
+  //     tomorrow: '123'
+  //   })
+  // }, 3000)
+
+
   return (
     <div>
       <h1>Users</h1>
